e05822a95a139b4eba96bd474d2b869a6335e812
Devanshu458268 <66779341+Devanshu458268@users.noreply.github.com>
2023-08-28 00:59:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var heart=document.querySelector("i")
+var container=document.querySelector(".container")
+
+container.addEventListener("dblclick",function(){
+    heart.style.opacity="1"
+    heart.style.transform="translate(-50%,-50%) scale(2)"
+   
+    setTimeout(function(){
+        heart.style.opacity="0"
+    },1000)
+    setTimeout(function(){
+        heart.style.transform="translate(-50%,-50%) scale(1)"
+    },1500)
+})
+
